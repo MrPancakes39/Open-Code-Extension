23e2b74ef6ddcbe63a5d83ecf8b733b5fe41f589
Salman Hasan <salmannhassan39@gmail.com>
2022-02-20 16:14:20 +0200

    Created "Open with Code" Caja Extension

---
+++ open-with-code.py
@@ -0,0 +1,47 @@
+# "Open with Code" extension is made by Salman Hasan
+#  based on open-terminal.py example by Martin Enlund
+
+import os
+
+from gi.repository import Caja, GObject
+
+
+class OpenCodeExtension(Caja.MenuProvider, GObject.GObject):
+    def __init__(self):
+        pass
+
+    def _open_code(self, file, isDir):
+        filename = file.get_location().get_path()
+        os.chdir(filename)
+
+        if isDir:
+            os.system("code . &")
+        else:
+            os.system("code %s &" % filename)
+
+    def menu_activate_cb(self, menu, file):
+        self._open_code(file, False)
+
+    def menu_background_activate_cb(self, menu, file):
+        self._open_code(file, True)
+
+    def get_file_items(self, window, files):
+        if len(files) != 1:
+            return
+
+        file = files[0]
+        if not file.is_directory() or file.get_uri_scheme() != "file":
+            return
+
+        item = Caja.MenuItem(name="CajaPython::opencode_file_item",
+                             label="Open with Code",
+                             tip="Open Code In %s" % file.get_name())
+        item.connect("activate", self.menu_activate_cb, file)
+        return [item]
+
+    def get_background_items(self, window, file):
+        item = Caja.MenuItem(name="CajaPython::opencode_item",
+                             label="Open with Code",
+                             tip="Open Code In This Directory")
+        item.connect("activate", self.menu_background_activate_cb, file)
+        return [item]
